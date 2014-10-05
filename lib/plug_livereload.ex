7e38f119dd860a6324e8b981360e18f777d1c828
Gabriel Jaldon <gjaldon85@gmail.com>
2014-10-05 19:27:24 +0800

    Insert livereload script

--- lib/plug_livereload.ex
+++ lib/plug_livereload.ex
@@ -1,2 +1,30 @@
 defmodule PlugLivereload do
+  alias Plug.Conn
+
+  @livereload_path Path.expand("js/livereload.js")
+  @livereload_port 3579
+
+  def init([]) do
+  end
+
+  def call(conn, _) do
+    insert_script(conn)
+  end
+
+  def insert_script(conn) do
+    {:ok, body, conn} = Conn.read_body(conn)
+    new_body = String.replace(body, ~r/(<head>|<head[^(er)][^<]*>)/, "\\1\n#{template(conn)}", global: false)
+    Conn.resp(conn, 200, new_body)
+  end
+
+  def template(conn) do
+    EEx.eval_file "template/livereload.html.eex", [
+      livereload_source: livereload_source(conn),
+      livereload_port: @livereload_port
+    ]
+  end
+
+  def livereload_source(conn) do
+    "#{@livereload_path}?host=#{conn.host}"
+  end
 end
